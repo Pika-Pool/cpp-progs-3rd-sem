3589349a12689b6bf9c1b7b5e49c6a73c95d298e
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2020-12-10 16:23:43 +0530

    87 - add comments

--- 87-use put() to file ascii.cpp
+++ 87-use put() to file ascii.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 int main()
 {
+	// object to read and right files
 	fstream file;
 
+	// open file in output mode
 	file.open("Aschars.txt", ios::out);
 
+	// store ascii characters from 33-127 into file
 	for (int i = 33; i <= 127; ++i)
 	{
 		file.put(char(i));
@@ -18,8 +21,10 @@ int main()
 
 	cout << "Text in file  -  Aschars.txt:\n";
 
+	// open file in input mode to read
 	file.open("Aschars.txt", ios::in);
 
+	// read each character and print to console
 	char c;
 	while (file.get(c))
 	{
