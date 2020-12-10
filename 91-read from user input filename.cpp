4cb64609ce145d0c51b8b08bd64fe88edcaf030d
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2020-12-10 16:26:30 +0530

    91 - add comments

--- 91-read from user input filename.cpp
+++ 91-read from user input filename.cpp
@@ -5,9 +5,12 @@ using namespace std;
 int main()
 {
 	char filename[FILENAME_MAX];
+	// get filename as input from user
 	cout << "Enter file name to read from:\t";
 	cin.getline(filename, FILENAME_MAX, '\n');
 
+	// read character by character and print
+	// contents of file to console
 	cout << "\nCONTENTS IN FILE============================================================:\n";
 	char c;
 	ifstream file(filename);
