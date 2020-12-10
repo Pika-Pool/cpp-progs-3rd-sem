98f9dda55650f6afd12d8aa52ffbea489fd4b126
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2020-12-10 16:28:52 +0530

    93 - add comments

--- 93-count chars and digs in file.cpp
+++ 93-count chars and digs in file.cpp
@@ -7,14 +7,18 @@ int main()
 {
 	cout << "===COUNT CHARACTERS AND DIGITS IN FILE===\n";
 
+	// get filename from user
 	char filename[FILENAME_MAX];
 	cout << "\nEnter filename to read:\t";
 	cin.getline(filename, FILENAME_MAX, '\n');
 
+	// object to read from file
 	ifstream file(filename);
 	int charCount, digCount, unknownCount;
 	charCount = digCount = unknownCount = 0;
 
+	// read character by character and check
+	// if its a letter or digit or unknown value
 	char c;
 	while (file.get(c))
 	{
@@ -25,6 +29,9 @@ int main()
 		else
 			unknownCount++;
 	}
+
+	// don't forget to close the file to avoid corruption
+	file.close();
 
 	cout << "\nResult:";
 	cout << "\nCharacters:\t" << charCount;
