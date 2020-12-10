4a3ef74e9eff123541b2486f36dcab779e678ed2
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2020-12-10 16:03:06 +0530

    77 - add comments

--- 77-class template with more than 1 generic type.cpp
+++ 77-class template with more than 1 generic type.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+// class template with >1 generic type
+// KeyValue stores a pair of data together
 template <typename T1, typename T2>
 class KeyValue
 {
@@ -10,6 +12,7 @@ class KeyValue
 public:
 	KeyValue(T1 k, T2 v) : key(k), value(v) {}
 
+	// overload << opertor to print object of this class
 	friend ostream &operator<<(ostream &out, const KeyValue &kv)
 	{
 		out << kv.key << " : ( " << kv.value << " )";
@@ -19,8 +22,11 @@ public:
 
 int main()
 {
+	// INT AND CHAR
 	KeyValue<int, char> a(21, 'l');
+	// CHAR AND FLOAT
 	KeyValue<char, float> b('j', 43.22);
+	// INT AND KeyValue which further stores CHAR AND CHAR
 	KeyValue<int, KeyValue<char, char>> c(21, KeyValue<char, char>('a', 'z'));
 
 	cout << "Key : ( Value ) pairs:\n\n";
