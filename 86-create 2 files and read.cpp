fffdf5d480660ba9e47dc24703d71b6633e4883e
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2020-12-10 16:22:07 +0530

    86 - add comments

--- 86-create 2 files and read.cpp
+++ 86-create 2 files and read.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int main()
 {
 	const int size = 2;
-	ofstream filesWrite[size];
+	// object to write to file
+	ofstream filesWrite;
 
 	for (int i = 0; i < size; ++i)
 	{
@@ -15,25 +16,28 @@ int main()
 		cout << "Enter contents for \"file - " << i + 1 << "\":\n";
 		cin.getline(str, 50, '\n');
 
-		filesWrite[i].open("file - " + to_string(i + 1) + ".txt");
-		filesWrite[i] << str << "\n";
-		filesWrite[i].close();
+		// open file and write user input to it
+		filesWrite.open("file - " + to_string(i + 1) + ".txt");
+		filesWrite << str << "\n";
+		filesWrite.close();
 
 		cout << "Contents written to file!!\n\n";
 	}
 
 	cout << "\n";
 
-	ifstream filesRead[size];
+	// object to read from file
+	ifstream filesRead;
 	for (int i = 0; i < size; ++i)
 	{
 		char str[50];
 
 		cout << "Contents of \"file - " << i + 1 << "\":\n";
 
-		filesRead[i].open("file - " + to_string(i + 1) + ".txt");
-		filesRead[i].getline(str, 50, '\n');
-		filesRead[i].close();
+		// open file, read and print contents inside
+		filesRead.open("file - " + to_string(i + 1) + ".txt");
+		filesRead.getline(str, 50, '\n');
+		filesRead.close();
 
 		cout << str << '\n';
 	}
