d84856a2453b95f8cd18c773928faf8ba124aa6c
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2020-12-10 15:55:24 +0530

    75 - add comments

--- 75-function template more than 1 generic type arrgument.cpp
+++ 75-function template more than 1 generic type arrgument.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
 
+// template taking multiple arguments
+// with more than 1 type
 template <typename T1, typename T2>
 void display(T1 a, T2 b)
 {
+	// display the gicen arguments
 	cout << "Argument - 1"
 		 << "  :  " << a << "\n";
 	cout << "Argument - 2"
@@ -25,21 +28,27 @@ int main()
 	cout << "Enter a character:\t";
 	cin >> c;
 
+	// CALL FUNCTION WITH int AND floar
 	cout << "int, float\n";
 	display(i, f);
 
+	// CALL FUNCTION WITH int AND char
 	cout << "int, char\n";
 	display(i, c);
 
+	// CALL FUNCTION WITH float AND int
 	cout << "float, int\n";
 	display(f, i);
 
+	// CALL FUNCTION WITH float AND char
 	cout << "float, char\n";
 	display(f, c);
 
+	// CALL FUNCTION WITH char AND int
 	cout << "char, int\n";
 	display(c, i);
 
+	// CALL FUNCTION WITH char AND float
 	cout << "char, float\n";
 	display(c, f);
 
