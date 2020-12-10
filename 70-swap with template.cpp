db10156ce31ddce9d425cef4e8dc785e3eab8ec2
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2020-12-10 12:17:53 +0530

    70 - add comments

--- 70-swap with template.cpp
+++ 70-swap with template.cpp
@@ -1,36 +1,45 @@
 #include <iostream>
+using std::cin, std::cout;
 
+// function swaps 2 references of any type
+// a generic type T
 template <typename T>
-void swap(T &a, T &b) {
+void swap(T &a, T &b)
+{
 	T temp = a;
 	a = b;
 	b = temp;
 }
 
-int main() {
+int main()
+{
+	// SWAP INTEGERS
 	int i1, i2;
-	std::cout << "Enter 2 integers:\t";
-	std::cin >> i1 >> i2;
+	cout << "Enter 2 integers:\t";
+	cin >> i1 >> i2;
 	swap(i1, i2);
-	std::cout << "After swapping:\t" << i1 << " " << i2;
+	cout << "After swapping:\t" << i1 << " " << i2;
 
+	// SWAP FLOATS
 	float f1, f2;
-	std::cout << "\n\nEnter 2 floats:\t";
-	std::cin >> f1 >> f2;
+	cout << "\n\nEnter 2 floats:\t";
+	cin >> f1 >> f2;
 	swap(f1, f2);
-	std::cout << "After swapping:\t" << f1 << " " << f2;
+	cout << "After swapping:\t" << f1 << " " << f2;
 
+	// SWAP CHARS
 	char c1, c2;
-	std::cout << "\n\nEnter 2 chars:\t";
-	std::cin >> c1 >> c2;
+	cout << "\n\nEnter 2 chars:\t";
+	cin >> c1 >> c2;
 	swap(c1, c2);
-	std::cout << "After swapping:\t" << c1 << " " << c2;
+	cout << "After swapping:\t" << c1 << " " << c2;
 
+	// SWAP LONG LONG
 	long long l1, l2;
-	std::cout << "\n\nEnter 2 longs:\t";
-	std::cin >> l1 >> l2;
+	cout << "\n\nEnter 2 longs:\t";
+	cin >> l1 >> l2;
 	swap(l1, l2);
-	std::cout << "After swapping:\t" << l1 << " " << l2;
+	cout << "After swapping:\t" << l1 << " " << l2;
 
 	return 0;
 }
